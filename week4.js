6a773051ed5b350adece1821683c69d8ecb32c52
Ellis-D13 <135081698+Ellis-D13@users.noreply.github.com>
2023-07-16 13:25:08 -0500

    fin

--- week4.js
+++ week4.js
@@ -50,20 +50,20 @@ for (let i = 0; i < names.length; i++) {
 console.log(nameLengths);
 
 // write a loop to iterate over the nameLenghts array and calcuate the sum of all the elements in the array. 
-let nameLenghts = [3, 5, 3, 5, 4, 3];
+{let nameLengths = [3, 5, 3, 5, 4, 3];
 {
     let sum = 0; 
 
-    for (let i = 0; i < nameLenghts.length; i++) {
-        sum += nameLenghts[i];
+    for (let i = 0; i < nameLengths.length; i++) {
+        sum += nameLengths[i];
     }
     let result = sum; 
     console.log("Sum of name lengths:", result);
 }
-
+}
 // write a function that takes two parameters, word and n, as aguments and retursn the work concatenated to itself n number of times. 
 function concatenateWord(word, n) {
-    let concatenateWord = ``;
+    let concatenateWord = '';
 
     for (let i = 0; i < n; i++) {
         concatenateWord += word; 
@@ -72,34 +72,39 @@ function concatenateWord(word, n) {
 }
 
 // Example usage
-function example() {
-    let result = concatenateWord('Hello', 3);
-    console.log(result);
+// 'example' function used to create a new scope and create a new delcared 'result'
+{
+    function example() {
+        let result = concatenateWord('Hello', 3);
+        console.log(result);
+    }
 }
-
+// variables declared within scope doesn't conflict with previous executions
 example();
-
 // write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by space. 
-
+// getFullName function takes firstName and lastName as parameters and concats with a `' '` space using the '+' operator
 function getFullName(firstName, lastName) {
     return firstName + " " + lastName;
 }
-// in use
+// in use the function concats the first and last names together with a space to create the full name
 let fullName = getFullName('John', 'Doe');
+// prints to the console 
 console.log(fullName); 
 
 // Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
-
+// the function initalizes a variable 'sum' to 0. 
 function isSumGreaterThan100(numbers) {
     let sum = 0; 
-
+// using a for loop to iterate over the 'numbers' array and add each number to the 'sum' variable
+// keyword 'for', initialization 'let i = 0', condition 'i < numbers.length', update 'i++' or the increment operator 
     for (let i = 0; i < numbers.length; i++) {
         sum += numbers[i]; 
     }
+// using a comparison ('sum . 100') to check the sum of all nums is > 100   
     return sum > 100; 
 }
 
-// example usage
+// pass an array [20, 30,60] to the 'isSumGreaterThan100' function. The total is 110 so it prints 'true'
 {
 let numbers = [20, 30, 60];
 let result = isSumGreaterThan100(numbers);
@@ -107,15 +112,17 @@ console.log(result);
 }
 
 // Write a function that takes an array of numbers and returns the average of all the elements in the array.
-
+// function initalizes variable 'sum' to 0. 
 function calculateAverage(numbers) {
     let sum = 0; 
+// Using a 'for' loop run through the 'numbers' array and add each number to the 'sum' variable    
     for (let i = 0; i < numbers.length; i++) {
         sum += numbers[i];
     }
+// return the aveage by dividing 'sum' by the length of the 'numbers' array 
     return sum / numbers.length; 
 }
-// example usage
+// pass an array '[10, 20, 30, 40, 50]' to 'calculateAverage' function 
 let numbers = [10, 20, 30, 40, 50];
 { 
     let sum = 0; 
@@ -126,3 +133,67 @@ let numbers = [10, 20, 30, 40, 50];
     let average = sum / numbers.length; 
     console.log(average);
 }
+
+//Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+// the function creates variables 'sum1' and 'sum2' to 0 to be used to calculate the sums of 'arr1' and 'arr2'
+function isAverageGreaterThan(arr1, arr2) {
+    let sum1 = 0; 
+    let sum2 = 0;
+// separate loops to go over again each array and calculate sums
+// loop one 'arr1'
+    for (let i = 0; i < arr1.length; i++) {
+        sum1 += arr1[i];
+    }
+// loop two 'arr2'    
+    for (let i = 0; i < arr2.length; i++) {
+        sum2 += arr2[i];
+    }
+// taking the sums and calculating the averages by dividing the sums by the lengths    
+    let average1 = sum1 / arr1.length; 
+    let average2 = sum2 / arr2.length; 
+// compare the two averages by using the '>' operator and return 'true' if average of 'arr1' < 'arr2'
+    return average1 > average2; 
+}
+// Passing the two arrays to the 'isAverageGreaterThan' function
+// elements in array1 average greater than array2 so the function prints 'true'
+{
+let array1 = [10, 20, 30];
+let array2 = [5, 15, 25];
+let result = isAverageGreaterThan(array1, array2);
+console.log(result); }
+
+// Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+// function 'willBuyDrink' uses 'isHotOutside' and 'moneyInPocket' as parameters the '&&' operator checks to see if both conditions are true: 'IsHotOutside' is 
+// 'true' and 'moneyInPocket' is greater than 10.50
+function willBuyDrink(isHotOutside, moneyInPocket) {
+    return isHotOutside && moneyInPocket > 10.50;
+}
+// if both conditions are satisfied the function returns 'true' otherwise will return 'false'
+// Below I use 'true' for 'isHot' and '15.75' for 'money' - which is greater than 10.50 for 'moneyInPocket' 
+{
+let isHot = true; 
+let money = 15.75;
+let result = willBuyDrink(isHot, money); }
+// both coditions are met so the function returns 'true' when printed to the console.
+console.log(result); 
+
+// Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+function getUniqueElements(array) {
+    //create an empty object to store unique elements
+    let uniqueElements = {}
+
+    // Iterate over the input array
+    for (let i = 0; i < array.length; i++) {
+        // use the elements as keys in the object to store unique values
+        uniqueElements[array[i]] = true; 
+    }
+    // extract the keys from the object to get the unique elements
+    let result = Object.keys(uniqueElements);
+
+    // return the unique elements as a new array
+    return result;
+}
+// This function solves the problem of extracting only the unique elements from an array and returning them in a new array. You can remove duplicate elements and work 
+//with only the distinct values. Using an object as a hash table you go over the input arry and store each element as a key in the object. Object keys are 
+//unique, dupicates will overwrite the previous occurrance with only the unique elements being stored. Then the keys are extracted using 'Object.keys' and 
+//return them as a new array
