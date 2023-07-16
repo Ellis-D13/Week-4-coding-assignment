2e6f9850f5061e6975cfcb059b6fbf79a341171d
Ellis-D13 <135081698+Ellis-D13@users.noreply.github.com>
2023-07-15 21:42:10 -0500

    updated code not complete.... needs annotation

--- week4.js
+++ week4.js
@@ -16,3 +16,95 @@ let sum = 0;
 }
 let average = sum / ages.length; 
 console.log("Average age:", average);
+
+// create an array with names
+var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+// calculate average number of letters per name
+var totalLetters = 0; 
+for (var i = 0; i < names.length; i++) {
+    totalLetters += names[i].length; 
+}
+var averageLetters = totalLetters / names.length; 
+
+console.log("Average number of letters per name: " + averageLetters); 
+// concat all the names together, separated by spaces
+var concatenatedNames = names.join(` `); 
+console.log("Concatenated names: " + concatenatedNames);
+
+//access the last element in an array
+var array = [1, 2, 3, 4, 5]; 
+var lastElement = array[array.length - 1];
+console.log(lastElement); 
+
+//access the first element in an array
+var array = [1, 2, 3, 4, 5];
+var firstElement = array[0];
+console.log(firstElement);
+
+//create a new name array called nameLengths
+ let nameLengths = [];
+
+for (let i = 0; i < names.length; i++) {
+    nameLengths.push(names[i].length);
+}
+console.log(nameLengths);
+
+// write a loop to iterate over the nameLenghts array and calcuate the sum of all the elements in the array. 
+let nameLenghts = [3, 5, 3, 5, 4, 3];
+{
+    let sum = 0; 
+
+    for (let i = 0; i < nameLenghts.length; i++) {
+        sum += nameLenghts[i];
+    }
+    let result = sum; 
+    console.log("Sum of name lengths:", result);
+}
+
+// write a function that takes two parameters, word and n, as aguments and retursn the work concatenated to itself n number of times. 
+function concatenateWord(word, n) {
+    let concatenateWord = ``;
+
+    for (let i = 0; i < n; i++) {
+        concatenateWord += word; 
+    }
+    return concatenateWord;
+}
+
+// Example usage
+function example() {
+    let result = concatenateWord('Hello', 3);
+    console.log(result);
+}
+
+example();
+
+// write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by space. 
+
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+// in use
+let fullName = getFullName('John', 'Doe');
+console.log(fullName); 
+
+// Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+
+function isSumGreaterThan100(numbers) {
+    let sum = 0; 
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]; 
+    }
+    return sum > 100; 
+}
+
+// example usage
+{
+let numbers = [20, 30, 60];
+let result = isSumGreaterThan100(numbers);
+console.log(result);
+}
+
+// Write a function that takes an array of numbers and returns the average of all the elements in the array.
+
